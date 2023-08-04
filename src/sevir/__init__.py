56f46575f84a10193cbdbe1a9a24e1979b3830c1
TSgt Jason Leaver <jason.leaver@us.af.mil>
2023-08-03 20:26:36 -0500

    🔥🐍

--- src/sevir/__init__.py
+++ src/sevir/__init__.py
@@ -5,12 +5,12 @@ __all__ = [
     "H5File",
     "H5Store",
     "catalog",
-    "dataset",
+    "datasets",
     "constants",
     "h5",
     "display",
 ]
-from . import catalog, constants, dataset, display, h5
+from . import catalog, constants, datasets, display, h5
 from .catalog import Catalog
-from .dataset import TensorGenerator, TensorLoader
+from .datasets import TensorGenerator, TensorLoader
 from .h5 import H5File, H5Store
